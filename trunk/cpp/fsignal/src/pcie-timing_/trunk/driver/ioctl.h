7efb6e865fe575b592a78c70161897085b747773
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-02-18 18:32:47 +0000

    Initial import
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- trunk/cpp/fsignal/src/pcie-timing_/trunk/driver/ioctl.h
+++
@@ -1,6 +0,0 @@
-#ifndef _KX1PCIEDRIVER_IOCTL_H
-#define _KX1PCIEDRIVER_IOCTL_H
-
-long _unlocked_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
-
-#endif /* _KX1PCIEDRIVER_IOCTL_H */
