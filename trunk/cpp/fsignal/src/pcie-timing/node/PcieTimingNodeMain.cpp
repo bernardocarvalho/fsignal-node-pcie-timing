b9797d60a12cf65f4c9b37992052c46fde32a932
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-02-23 17:41:30 +0000

    Making new node
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- trunk/cpp/fsignal/src/pcie-timing/node/PcieTimingNodeMain.cpp
+++ trunk/cpp/fsignal/src/pcie-timing/node/PcieTimingNodeMain.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2017 by Andre Neto / B. Carvalho  *
- *   andre.neto@ipfn.ist.utl.pt   *
+ *   Copyright (C) 2017 by Andre Neto / B. Carvalho                        *
+ *   andre.neto@ipfn.ist.utl.pt                                            *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
